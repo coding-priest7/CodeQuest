fa2736072b89811e57ad7e66f75d068a7162db6f
coding-priest7 <sandeep17goswami@gmail.com>
2023-06-19 21:40:30 +0530

    added changes to footer

--- prepare.py
+++ prepare.py
@@ -1,34 +1,67 @@
-# read the index.txt and prepare documents, vocab , idf
+import os
+import re
 
-import chardet
+qData_folder = "Qdata"
+
+# We are taking the body of the question, so we stop reading once we encounter examples
+target_str = "Example 1:"
+
+all_lines = []
+
+for i in range(1, 2177):
+    # adding body of the ith question
+    file_path = os.path.join(qData_folder, "{}/{}.txt".format(i, i))
+
+    doc = ""
+    with open(file_path, "r", encoding='utf-8', errors="ignore") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if target_str in line:
+            break
+        else:
+            doc += line
+
+    all_lines.append(doc)
 
 
-def find_encoding(fname):
-    r_file = open(fname, 'rb').read()
-    result = chardet.detect(r_file)
-    charenc = result['encoding']
-    return charenc
+# adding heading of the ith question
+head_path = os.path.join(qData_folder, "index.txt")
+
+with open(head_path, "r", encoding='utf-8', errors="ignore") as f:
+    headings = f.readlines()
+
+for (i, heading) in enumerate(headings, 0):
+    try:
+        # removing the Q No from the heading
+        words = heading.split()
+        if len(words) >= 2:
+            heading = ' '.join(words[1:])
+            # Now adding the remaining words in the heading to the respective q's body
+            all_lines[i] += heading
+        else:
+            # Handle the case when there are not enough words in the heading
+            print("Error: Not enough words in the heading.")
+    except IndexError:
+        # Handle the IndexError here
+        print("Error: Index out of range occurred.")
 
 
-filename = 'Qdata/index.txt'
-my_encoding = find_encoding(filename)
+def preprocess(text):      # remove problem no, and return a list of lowercase words
+    # removing non alphanumeric chars
+    text = re.sub(r'[^a-zA-Z0-9\s-]', '', text)
+    terms = [term.lower() for term in text.strip().split()]
 
-with open(filename, 'r', encoding=my_encoding) as f:
-    lines = f.readlines()
-
-
-def preprocess(document_text):
-    # remove the leading numbers from the string, remove not alpha numeric characters, make everything lowercase
-    terms = [term.lower() for term in document_text.strip().split()[1:]]
     return terms
 
 
-vocab = {}
-documents = []
-for index, line in enumerate(lines):
-    # read statement and add it to the line and then preprocess
-    tokens = preprocess(line)
+vocab = {}          # word : no of docs that word is present in
+documents = []      # all lists, with each list containing words of a document
+
+for (index, line) in enumerate(all_lines):
+    tokens = preprocess(line)  # list of processed words of this doc
     documents.append(tokens)
+
     tokens = set(tokens)
     for token in tokens:
         if token not in vocab:
@@ -36,40 +69,46 @@ for index, line in enumerate(lines):
         else:
             vocab[token] += 1
 
-# reverse sort the vocab by the values
+# reverse sort vocab by values (by the no of docs the word is present in)
 vocab = dict(sorted(vocab.items(), key=lambda item: item[1], reverse=True))
 
-print('Number of documents: ', len(documents))
-print('Size of vocab: ', len(vocab))
-print('Sample document: ', documents[0])
 
-# save the vocab in a text file
-with open('TF-IDF/vocab.txt', 'w') as f:
+print("No of documents : ", len(documents))
+print("Size of vocab : ", len(vocab))
+print("Sample document: ", documents[100])
+
+# keys of vocab thus is a set of distinct words across all docs
+# save them in file vocab
+with open("TF-IDF/vocab.txt", "w", encoding='utf-8', errors="ignore") as f:
     for key in vocab.keys():
         f.write("%s\n" % key)
 
-# save the idf values in a text file
-with open('TF-IDF/idf-values.txt', 'w') as f:
+# save idf values
+with open("TF-IDF/idf-values.txt", "w", encoding='utf-8', errors="ignore") as f:
     for key in vocab.keys():
         f.write("%s\n" % vocab[key])
 
-# save the documents in a text file
-with open('TF-IDF/documents.txt', 'w') as f:
-    for document in documents:
-        f.write("%s\n" % ' '.join(document))
 
+# save the documents(lists of words for each doc)
+with open("TF-IDF/documents.txt", "w", encoding='utf-8', errors="ignore") as f:
+    for doc in documents:
+        f.write("%s\n" % doc)
 
+# word : list of index of docs the word is present in.
 inverted_index = {}
-for index, document in enumerate(documents):
-    for token in document:
+# inserting word multiple times from same doc too, so that we even get the term freq from here itself
+for (index, doc) in enumerate(documents, start=1):
+    for token in doc:
         if token not in inverted_index:
             inverted_index[token] = [index]
         else:
             inverted_index[token].append(index)
 
-# save the inverted index in a text file
-with open('TF-IDF/inverted-index.txt', 'w') as f:
+
+# save the inverted index in a file
+with open("TF-IDF/inverted-index.txt", 'w', encoding='utf-8', errors="ignore") as f:
     for key in inverted_index.keys():
         f.write("%s\n" % key)
-        f.write("%s\n" % ' '.join([str(doc_id)
-                for doc_id in inverted_index[key]]))
+
+        doc_indexes = ' '.join([str(term) for term in inverted_index[key]])
+        f.write("%s\n" % doc_indexes)
